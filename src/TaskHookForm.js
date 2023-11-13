82837ac6e4640f700b2e186925466b54463681e5
epgokce <ep.gokce@gmail.com>
2023-11-14 00:05:31 +0300

    Initial setup: TaskHookForm component

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import { nanoid } from "nanoid";
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
-export default function TaskHookForm() {
+
+export default function TaskHookForm({kisiler, submitFn}) {
+
+  
   return (
     <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
   )
