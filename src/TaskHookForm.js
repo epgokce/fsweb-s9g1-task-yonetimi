b6fa41fc40c9d3c50836dd0b29699ca2bac9503f
epgokce <ep.gokce@gmail.com>
2023-11-14 00:10:09 +0300

    Set up form structure

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React from "react";
 import { nanoid } from "nanoid";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 
+export default function TaskHookForm({ kisiler, submitFn }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+    reset,
+  } = useForm({
+    defaultValues: {
+      id: "", // TaskForm.js'te de bu şekilde verilmiş.
+      title: "",
+      description: "",
+      people: [],
+    },
+    mode: "onChange",
+  });
 
-export default function TaskHookForm({kisiler, submitFn}) {
+  // handleSubmit içerisindeki onSubmit / handleSubmit(mySubmit):
 
-  
+  function mySubmit(formData) {
+    console.log("formData:", formData);
+    toast.success("Task Eklendi! Hadi Göreve!!!");
+    submitFn({
+      ...formData,
+      id: nanoid(5),
+      status: "yapılacak",
+    });
+    reset();
+  }
   return (
-    <div>Formunuzu react-hook-form kullanarak burada oluşturun. TaskForm dosyasındaki HTML yapısını vs app.css içerisindeki classları kullanabilirsiniz.</div>
-  )
+    <div>
+      <form className="taskForm" onSubmit={handleSubmit(mySubmit)}>
+        <div className="form-line">
+          <label className="input-label" htmlFor="title">
+            Başlık
+          </label>
+        </div>
+      </form>
+    </div>
+  );
 }
