a8a55a22fae2583b6e29b8cb4b93c433ce93b3e8
epgokce <ep.gokce@gmail.com>
2023-11-14 00:14:49 +0300

    Add form validation

--- src/TaskHookForm.js
+++ src/TaskHookForm.js
@@ -38,6 +38,38 @@ export default function TaskHookForm({ kisiler, submitFn }) {
           <label className="input-label" htmlFor="title">
             Başlık
           </label>
+          <input
+            className="input-text"
+            id="title"
+            type="text"
+            {...register('title', {
+              required: 'Task başlığı yazmalısınız',
+              minLength: { value: 3, message: 'Task başlığı en az 3 karakter olmalı' },
+            })}
+          />
+          {errors.title && <p className="input-error">{errors.title.message}</p>}
+        </div>
+
+        <div className="form-line">
+          <label className="input-label" htmlFor="description">
+            Açıklama
+          </label>
+          <textarea
+            className="input-textarea"
+            rows="3"
+            id="description"
+            {...register('description', {
+              required: 'Task açıklaması yazmalısınız',
+              minLength: { value: 10, message: 'Task açıklaması en az 10 karakter olmalı' },
+            })}
+          ></textarea>
+          {errors.description && <p className="input-error">{errors.description.message}</p>}
+        </div>
+
+        <div className="form-line">
+          <label className="input-label">İnsanlar</label>
+          <div>
+          </div>
         </div>
       </form>
     </div>
